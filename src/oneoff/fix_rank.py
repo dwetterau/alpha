8ef8104d132a4fa7f59b42205b46b8aa14866f7f
David Wetterau <david.wetterau@gmail.com>
2014-11-04 16:37:21 -0600

    fixed the script to fix the redis entries

--- src/oneoff/fix_rank.py
+++ src/oneoff/fix_rank.py
@@ -5,5 +5,5 @@ mysql_file = open(sys.argv[1], 'r')
 for line in mysql_file:
     split = line.strip().split(' ')
     value = (int(split[1]) / (12.0 * 60 * 60000))
-    print 'ZADD ir %d %s\r' % (value, split[0])
+    print 'ZADD ir %f %s\r' % (value, split[0])
 
